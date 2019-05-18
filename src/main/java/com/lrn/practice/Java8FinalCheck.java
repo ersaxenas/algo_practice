5977edbc958cf243d0d07cd9744e048813f8d224
Saurabh Saxena <saurabh.saxena@imanage.com>
2019-05-18 17:07:31 -0500

    temp checkin

--- src/main/java/com/lrn/practice/Java8FinalCheck.java
+++ src/main/java/com/lrn/practice/Java8FinalCheck.java
@@ -2,8 +2,8 @@ package com.lrn.practice;
 
 public class Java8FinalCheck {
 	
-	public void runMe(final String str) {
-		System.out.println("str");
+	public void runMe(String str) {
+		String abc = "final string"; // since the value of the variable is not changing then in java 8 byte code will make it final.
 	}
 
 }
